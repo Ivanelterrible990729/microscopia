fbfabf7d3cbd85c4e3ad020b06b3e7472295d85e
Ivanelterrible990729 <ivanalejandro290799@gmail.com>
2024-12-21 14:52:46 -0600

    Prueba de diseño en login

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,11 +5,18 @@
 @endsection
 
 @section('content')
-    <div @class([
-        'p-3 sm:px-8 relative h-screen lg:overflow-hidden bg-primary xl:bg-white dark:bg-darkmode-800 xl:dark:bg-darkmode-600',
-        'before:hidden before:xl:block before:content-[\'\'] before:w-[57%] before:-mt-[28%] before:-mb-[16%] before:-ml-[13%] before:absolute before:inset-y-0 before:left-0 before:transform before:rotate-[-4.5deg] before:bg-primary/20 before:rounded-[100%] before:dark:bg-darkmode-400',
-        'after:hidden after:xl:block after:content-[\'\'] after:w-[57%] after:-mt-[20%] after:-mb-[13%] after:-ml-[13%] after:absolute after:inset-y-0 after:left-0 after:transform after:rotate-[-4.5deg] after:bg-primary after:rounded-[100%] after:dark:bg-darkmode-700',
-    ])>
+<div @class([
+    'p-3 sm:px-8 relative h-screen lg:overflow-hidden bg-primary xl:bg-white dark:bg-darkmode-800 xl:dark:bg-darkmode-600',
+    'before:hidden before:xl:block before:content-[\'\'] before:w-[57%] before:-mt-[28%] before:-mb-[16%] before:-ml-[13%] before:absolute before:inset-y-0 before:left-0 before:transform before:rotate-[-4.5deg] before:bg-primary/20 before:rounded-[100%] before:dark:bg-darkmode-400',
+    'after:hidden after:xl:block after:content-[\'\'] after:w-[57%] after:-mt-[20%] after:-mb-[13%] after:-ml-[13%] after:absolute after:inset-y-0 after:left-0 after:transform after:rotate-[-4.5deg] after:bg-primary after:rounded-[100%] after:dark:bg-darkmode-700',
+])>
+    <!-- BG IMAGE -->
+    <img
+        src="{{ Vite::asset('resources/images/bg-image.jpg') }}"
+        alt="Imagen de fondo"
+        class="absolute inset-0 hidden xl:block w-[57%] -mt-[18%] -mb-[16%] -ml-[13%] transform rounded-[100%] opacity-30 z-10 object-cover"
+        />
+
         <div class="container relative z-10 sm:px-10">
             <div class="block grid-cols-2 gap-4 xl:grid">
                 <!-- BEGIN: Login Info -->
@@ -18,21 +25,24 @@
                         class="-intro-x flex items-center pt-5"
                         href=""
                     >
-                        <img
-                            class="w-6"
-                            src="{{ Vite::asset('resources/images/logo.svg') }}"
-                            alt="Midone - Tailwind Admin Dashboard Template"
+                        <div class="flex items-end">
+                            <img
+                            class="w-8"
+                            src="{{ Vite::asset('resources/images/ITRANS.png') }}"
+                            alt="ITRANS - Universidad de Guadalajara"
                         />
-                        <span class="ml-3 text-lg text-white"> Unidad de Microscopía </span>
+                        <span class="ml-3 text-lg text-white"> ITRANS</span>
+                        <span class="ml-2 text-base text-white">UDG</span>
+                        </div>
                     </a>
-                    <div class="my-auto">
+                    <div class="my-auto w-1/2 flex flex-col items-center">
                         <img
-                            class="-intro-x -mt-16 w-1/2"
+                            class="-intro-x -mt-16 w-1/4"
                             src="{{ Vite::asset('resources/images/logo.svg') }}"
-                            alt="Midone - Tailwind Admin Dashboard Template"
+                            alt="Microscopía ITRANS"
                         />
                         <div class="-intro-x mt-10 text-4xl font-medium leading-tight text-white">
-                            {{ config('app.name') }}
+                            <b>{{ config('app.name') }}</b>
                         </div>
                     </div>
                 </div>
@@ -41,69 +51,55 @@
                 <div class="my-10 flex h-screen py-5 xl:my-0 xl:h-auto xl:py-0">
                     <div
                         class="mx-auto my-auto w-full rounded-md bg-white px-5 py-8 shadow-md dark:bg-darkmode-600 sm:w-3/4 sm:px-8 lg:w-2/4 xl:ml-20 xl:w-auto xl:bg-transparent xl:p-0 xl:shadow-none">
-                        <div class="intro-x mt-2 text-center text-slate-400 xl:hidden">
-                            {{ config('app.name') }}
+                        <div class="intro-x xl:hidden">
+                            <div class="flex flex-col items-center">
+                                <x-base.lucide
+                                    class="copy-code mr-2 h-16 w-16"
+                                    icon="Microscope"
+                                />
+                                <div class="text-center text-slate-400 mt-2">
+                                    {{ config('app.name') }}
+                                </div>
+                            </div>
                         </div>
-                        <h2 class="intro-x text-center text-2xl font-bold xl:text-left xl:text-3xl">
+                        <h2 class="intro-x text-center text-2xl font-bold xl:text-left xl:text-3xl mt-5 xl:mt-0">
                             Inicio de sesión
                         </h2>
                         <div class="intro-x mt-8">
-                            {{-- <x-base.form-input
+                            <x-base.form-input
                                 class="intro-x block min-w-full px-4 py-3 xl:min-w-[350px]"
                                 type="text"
-                                placeholder="Email"
+                                placeholder="Correo electrónico"
                             />
                             <x-base.form-input
                                 class="intro-x mt-4 block min-w-full px-4 py-3 xl:min-w-[350px]"
                                 type="password"
-                                placeholder="Password"
-                            /> --}}
+                                placeholder="Contraseña"
+                            />
                         </div>
                         <div class="intro-x mt-4 flex text-xs text-slate-600 dark:text-slate-500 sm:text-sm">
                             <div class="mr-auto flex items-center">
-                                {{-- <x-base.form-check.input
+                                <x-base.form-check.input
                                     class="mr-2 border"
                                     id="remember-me"
                                     type="checkbox"
-                                /> --}}
+                                />
                                 <label
                                     class="cursor-pointer select-none"
                                     for="remember-me"
                                 >
-                                    Remember me
+                                    Recuérdame
                                 </label>
                             </div>
-                            <a href="">Forgot Password?</a>
+                            <a href="">¿Olvidó su contraseña?</a>
                         </div>
                         <div class="intro-x mt-5 text-center xl:mt-8 xl:text-left">
-                            {{-- <x-base.button
-                                class="w-full px-4 py-3 align-top xl:mr-3 xl:w-32"
+                            <x-base.button
+                                class="w-full px-4 py-3 align-top xl:mr-3"
                                 variant="primary"
                             >
-                                Login
+                                Iniciar Sesión
                             </x-base.button>
-                            <x-base.button
-                                class="mt-3 w-full px-4 py-3 align-top xl:mt-0 xl:w-32"
-                                variant="outline-secondary"
-                            >
-                                Register
-                            </x-base.button> --}}
-                        </div>
-                        <div class="intro-x mt-10 text-center text-slate-600 dark:text-slate-500 xl:mt-24 xl:text-left">
-                            By signin up, you agree to our
-                            <a
-                                class="text-primary dark:text-slate-200"
-                                href=""
-                            >
-                                Terms and Conditions
-                            </a>
-                            &
-                            <a
-                                class="text-primary dark:text-slate-200"
-                                href=""
-                            >
-                                Privacy Policy
-                            </a>
                         </div>
                     </div>
                 </div>
