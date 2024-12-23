c86e749a4ec308d5e6d445930e7779dad06ccb5a
Ivanelterrible990729 <ivanalejandro290799@gmail.com>
2024-12-22 18:16:38 -0600

    Crea MenuComposer

---
+++ src/app/Main/SideMenu.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Main;
+
+class SideMenu
+{
+    /**
+     * List of side menu items.
+     */
+    public static function menu(): array
+    {
+        return [
+            'dashboard' => [
+                'icon' => 'home',
+                'title' => __('Dashboard'),
+                'route_name' => 'dashboard',
+            ],
+
+            'divider',
+
+            'users' => [
+                'icon' => 'users',
+                'title' => __('Users'),
+                'route_name' => 'login',
+            ],
+            'roles' => [
+                'icon' => 'shield-ellipsis',
+                'title' => __('Roles'),
+                'route_name' => 'login',
+            ],
+
+            'divider',
+
+            'imagenes' => [
+                'icon' => 'images',
+                'title' => __('Image Management'),
+                'route_name' => 'login',
+            ],
+        ];
+    }
+}
