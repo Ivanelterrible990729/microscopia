72f6ceb90ef4ee575332e7c77cc36797e7f00022
Ivanelterrible990729 <ivanalejandro290799@gmail.com>
2025-01-08 17:51:46 -0600

    Un usuario tiene muchas imagenes

---
+++ src/app/Models/Image.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Image extends Model
+{
+    /** @use HasFactory<\Database\Factories\ImageFactory> */
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'images';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+    ];
+
+    /**
+     * Get the user that owns the Image
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
