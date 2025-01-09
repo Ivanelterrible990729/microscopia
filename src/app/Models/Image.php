e034337fe061136433ec55c6f59ca146a3a105b1
Ivanelterrible990729 <ivanalejandro290799@gmail.com>
2025-01-08 18:50:30 -0600

    Crea imagenes y las relaciona con labels

--- src/app/Models/Image.php
+++ src/app/Models/Image.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Image extends Model
@@ -39,5 +40,15 @@ class Image extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * The labels that belong to the Image
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function labels(): BelongsToMany
+    {
+        return $this->belongsToMany(Label::class, 'image_label', 'label_id', 'image_id');
     }
 }
