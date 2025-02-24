ea712f92d4a58844e01749733445a360c5bbdb32
Ivanelterrible990729 <ivanalejandro290799@gmail.com>
2025-02-23 22:39:53 -0600

    Agregar comando de activitylog:clear.

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+// use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
+
+/**
+ * Limpieza del Activitylog del sistema.
+ */
+Artisan::command('activitylog:clean --force')->dailyAt('23:00');
